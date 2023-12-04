732063575e7ae6aeb4fa0010d9f6192a1974eff5
Meurig Freeman <me@meurig.com>
2023-12-04 10:42:13 +0000

    feat: answers to day02 part1

---
+++ day04/day04part1.py
@@ -0,0 +1,66 @@
+from typing import Tuple, Set, List
+
+import pytest
+from pathlib import Path
+
+
+def run():
+    with open(Path(__file__).parent / 'Input.txt') as f:
+        lines = f.read().split('\n')
+
+    result = calc_all(lines)
+    print(f'Day04 part 1: {result}')
+
+
+def seperate_numbers(line: str) -> Tuple[Set[int], List[int]]:
+    inputs = line.split(':')[1].split('|')
+    winners = { int(x) for x in inputs[0].strip().split(' ') if x != '' }
+    ours = [ int(x) for x in inputs[1].strip().split(' ') if x != '' ]
+    return winners, ours
+
+def test_seperate_numbers():
+    line = 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+    expected_winners = {41, 48, 83, 86, 17}
+    expected_ours = {83, 86, 6, 31, 17, 9, 48, 53}
+    winners, ours = seperate_numbers(line)
+    assert winners == expected_winners
+    assert ours == expected_ours
+
+def calc(value: str) -> int:
+    winners, ours = seperate_numbers(value)
+    matching_numbers = [ x for x in ours if x in winners ]
+    matches = len(matching_numbers)
+    points = 0 if matches == 0 else pow(2, matches - 1)
+    return points
+
+testdata = [
+    ('Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53', 8),
+    ('Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19', 2),
+    ('Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1', 2),
+    ('Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83', 1),
+    ('Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36', 0),
+    ('Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11', 0),
+]
+@pytest.mark.parametrize("value, expected", testdata)
+def test_calc(value: str, expected: int):
+    # arrange
+    # act
+    result = calc(value)
+    # assert
+    assert result == expected
+
+
+def calc_all(data: list) -> int:
+    total = 0
+    for x in data:
+        total += calc(x)
+    return total
+
+def test_calc_all():
+    data = [x for (x, y) in testdata]
+    actual = calc_all(data)
+    assert actual == 13
+
+
+if __name__ == '__main__':
+    run()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from day01 import day01part1, day01part2
 from day02 import day02part1, day02part2
 from day03 import day03part1, day03part2
+from day04 import day04part1
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     day02part2.run()
     day03part1.run()
     day03part2.run()
+    day04part1.run()
 
 
 if __name__ == '__main__':
