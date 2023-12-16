e60d2a45b25730c410b58b6081442160d37b4476
Meurig Freeman <me@meurig.com>
2023-12-16 22:49:10 +0000

    feat: answer to day16 part2

--- day16/day16.py
+++ day16/day16.py
@@ -1,6 +1,5 @@
 from enum import Enum, StrEnum
-
-import pytest
+from functools import cache
 from pathlib import Path
 
 
@@ -8,8 +7,11 @@ def run():
     with open(Path(__file__).parent / 'Input.txt') as f:
         lines = f.read().split('\n')
 
-    result = count_energised_tiles(lines)
-    print(f'DayX part 1: {result}')
+    result = count_energised_tiles(lines, (0,0), Heading.RIGHT)
+    print(f'Day16 part 1: {result}')
+
+    result2 = get_max_energised_tiles(lines)
+    print(f'Day16 part 2: {result2}')
 
 class Heading(Enum):
     RIGHT = 1
@@ -24,72 +26,78 @@ class Tile(StrEnum):
     VERT_SPLIT = '|'
     HORIZ_SPLIT = '-'
 
-def in_bounds(position: (int, int), lines: list[str]) -> bool:
+@cache
+def in_bounds(position: (int, int), col_length: int, row_length: int) -> bool:
     x, y = position
-    ok_top_and_bottom = 0 <= x < len(lines)
-    ok_left_and_right = 0 <= y < len(lines[0])
+    ok_top_and_bottom = 0 <= x < col_length
+    ok_left_and_right = 0 <= y < row_length
     return ok_top_and_bottom and ok_left_and_right
 
-def move(position: (int, int), direction: Heading) -> (int, int):
+@cache
+def move(position: (int, int), direction: Heading) -> ((int, int), Heading):
     x, y = position
     match direction:
         case Heading.RIGHT:
-            return x, y + 1
+            return (x, y + 1), direction
         case Heading.LEFT:
-            return  x, y - 1
+            return  (x, y - 1), direction
         case Heading.DOWN:
-            return x + 1, y
+            return (x + 1, y), direction
         case Heading.UP:
-            return x - 1, y
+            return (x - 1, y), direction
     raise ValueError(f'unexpected heading: {direction}')
-
 
 def calc_new_positions(lines: list[str], start_pos: (int, int), direction: Heading) -> list[((int, int), Heading)]:
     x, y = start_pos
     current_char = lines[x][y]
     match current_char:
         case Tile.EMPTY:
-            return [(move(start_pos, direction), direction)]
+            return [move(start_pos, direction)]
         case Tile.MIRROR_RIGHT_UP:
             match direction:
                 case Heading.RIGHT:
-                    return [(move(start_pos, Heading.UP), Heading.UP)]
+                    return [move(start_pos, Heading.UP)]
                 case Heading.LEFT:
-                    return [(move(start_pos, Heading.DOWN), Heading.DOWN)]
+                    return [move(start_pos, Heading.DOWN)]
                 case Heading.DOWN:
-                    return [(move(start_pos, Heading.LEFT), Heading.LEFT)]
+                    return [move(start_pos, Heading.LEFT)]
                 case Heading.UP:
-                    return [(move(start_pos, Heading.RIGHT), Heading.RIGHT)]
+                    return [move(start_pos, Heading.RIGHT)]
         case Tile.MIRROR_RIGHT_DOWN:
             match direction:
                 case Heading.RIGHT:
-                    return [(move(start_pos, Heading.DOWN), Heading.DOWN)]
+                    return [move(start_pos, Heading.DOWN)]
                 case Heading.LEFT:
-                    return [(move(start_pos, Heading.UP), Heading.UP)]
+                    return [move(start_pos, Heading.UP)]
                 case Heading.DOWN:
-                    return [(move(start_pos, Heading.RIGHT), Heading.RIGHT)]
+                    return [move(start_pos, Heading.RIGHT)]
                 case Heading.UP:
-                    return [(move(start_pos, Heading.LEFT), Heading.LEFT)]
+                    return [move(start_pos, Heading.LEFT)]
         case Tile.VERT_SPLIT:
             match direction:
                 case (Heading.RIGHT | Heading.LEFT):
-                    return [ (move(start_pos, Heading.DOWN), Heading.DOWN),
-                             (move(start_pos, Heading.UP), Heading.UP)]
+                    return [ move(start_pos, Heading.DOWN),
+                             move(start_pos, Heading.UP)]
                 case (Heading.UP | Heading.DOWN):
-                    return [(move(start_pos, direction), direction)]
+                    return [move(start_pos, direction)]
         case Tile.HORIZ_SPLIT:
             match direction:
                 case (Heading.RIGHT | Heading.LEFT):
-                    return [(move(start_pos, direction), direction)]
+                    return [move(start_pos, direction)]
                 case (Heading.UP | Heading.DOWN):
-                    return [(move(start_pos, Heading.RIGHT), Heading.RIGHT),
-                            (move(start_pos, Heading.LEFT), Heading.LEFT)]
-def traverse(lines: list[str], current_pos: (int, int), direction: Heading, visited: set[((int, int), Heading)]) -> set[((int, int), Heading)]:
+                    return [move(start_pos, Heading.RIGHT),
+                            move(start_pos, Heading.LEFT)]
+
+def traverse(
+        lines: list[str], current_pos: (int, int), direction: Heading, visited: set[((int, int), Heading)]
+) -> set[((int, int), Heading)]:
     new_positions = calc_new_positions(lines, current_pos, direction)
     visited.add((current_pos, direction))
+    col_length = len(lines)
+    row_length = len(lines[0])
     while len(new_positions) == 1:
         new_position, new_heading = new_positions[0]
-        if in_bounds(new_position, lines):
+        if in_bounds(new_position, col_length, row_length):
             if (new_position, new_heading) not in visited:
                 visited.add((new_position, new_heading))
                 new_positions = calc_new_positions(lines, new_position, new_heading)
@@ -98,13 +106,12 @@ def traverse(lines: list[str], current_pos: (int, int), direction: Heading, visi
         else:
             new_positions = []
     for new_position, new_heading in new_positions:
-        if in_bounds(new_position, lines):
+        if in_bounds(new_position, col_length, row_length):
             if (new_position, new_heading) not in visited:
                 visited.update(traverse(lines, new_position, new_heading, visited))
     return visited
 
 def test_traverse():
-    print()
     lines = get_test_data()
     visited = traverse(lines, (0,0), Heading.RIGHT, set([]))
     actual = { position for position, heading in visited }
@@ -121,8 +128,8 @@ def test_traverse():
     assert (2, 2) not in actual
     assert (2, 3) not in actual
 
-def count_energised_tiles(lines: list[str]) -> int:
-    visited = traverse(lines, (0,0), Heading.RIGHT, set([]))
+def count_energised_tiles(lines: list[str], position: (int, int), heading: Heading) -> int:
+    visited = traverse(lines, position, heading, set([]))
     energised = {position for position, heading in visited}
     return len(energised)
 
@@ -149,7 +156,28 @@ def test_get_test_data():
 
 def test_count_energised_tiles():
     expected = 46
-    actual = count_energised_tiles(get_test_data())
+    actual = count_energised_tiles(get_test_data(), (0,0), Heading.RIGHT)
+    assert actual == expected
+
+def get_max_energised_tiles(rows: list[str]) -> int:
+    current_max = 0
+    last_row_index = len(rows) - 1
+    last_column_index = len(rows[0]) -1
+    for i in range(len(rows)):
+        # down the left side
+        current_max = max(current_max, count_energised_tiles(rows, (i, 0), Heading.RIGHT))
+        # down the right side
+        current_max = max(current_max, count_energised_tiles(rows, (i, last_column_index), Heading.LEFT))
+    for j in range(len(rows[0])):
+        # across the top
+        current_max = max(current_max, count_energised_tiles(rows, (0, j), Heading.DOWN))
+        # across the bottom
+        current_max = max(current_max, count_energised_tiles(rows, (last_row_index, j), Heading.UP))
+    return current_max
+
+def test_get_max_energised_tiles():
+    expected = 51
+    actual = get_max_energised_tiles(get_test_data())
     assert actual == expected
 
 if __name__ == '__main__':
